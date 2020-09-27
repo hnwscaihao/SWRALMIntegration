3f6cbe3e384a7459ac090f84acbf7367af6f27d1
Isxiaonian <369694235@qq.com>
2020-09-27 11:13:21 +0800

    添加checkData方法

--- src/main/java/com/sw/SWAPI/controller/AlmController.java
+++ src/main/java/com/sw/SWAPI/controller/AlmController.java
@@ -8,6 +8,7 @@ import com.sw.SWAPI.Error.MsgArgumentException;
 import com.sw.SWAPI.damain.Project;
 import com.sw.SWAPI.damain.User;
 import com.sw.SWAPI.util.*;
+import lombok.SneakyThrows;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,62 +33,63 @@ import static com.sw.SWAPI.util.ResultJson.*;
 public class AlmController {
 
 
-	public static final Log log = LogFactory.getLog(AlmController.class);
+    public static final Log log = LogFactory.getLog(AlmController.class);
 
 
-	String filePath = "C:\\\\Program Files\\\\Integrity\\\\ILMServer12\\\\data\\\\tmp";
+    String filePath = "C:\\\\Program Files\\\\Integrity\\\\ILMServer12\\\\data\\\\tmp";
 
-	@Value("${token}")
-	private String token;
+    @Value("${token}")
+    private String token;
 
-	private MKSCommand mks;
-	
-	private IntegrityUtil util = new IntegrityUtil();
-	/**
-	 * @Description
-	 * @Author liuxiaoguang
-	 * @Date 2020/7/16 15:33
-	 * @Param []
-	 * @Return com.alibaba.fastjson.JSONObject
-	 * @Exception 获取ALM中所有用户信息
-	 */
-	@RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
-	public JSONArray getAllUsers() {
+    private MKSCommand mks;
 
-		List<User> allUsers = new ArrayList<User>();
-		try {
-			log.info("开始链接：");
-			if (mks == null) {
-				mks = new MKSCommand();
-			}
-			allUsers = mks.getAllUsers(Arrays.asList("fullname", "name", "Email"));
-		} catch (APIException e) {
-			log.info("error: " + "查询所有用户错误！" + e.getMessage());
-			e.printStackTrace();
-		}
+    private IntegrityUtil util = new IntegrityUtil();
 
-		// mks.close(host,port,loginName);
+    /**
+     * @Description
+     * @Author liuxiaoguang
+     * @Date 2020/7/16 15:33
+     * @Param []
+     * @Return com.alibaba.fastjson.JSONObject
+     * @Exception 获取ALM中所有用户信息
+     */
+    @RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
+    public JSONArray getAllUsers() {
 
-		JSONArray jsonArray = new JSONArray();
-		for (int i = 0; i < allUsers.size(); i++) {
-			JSONObject jsonObject = new JSONObject();
-			User user = allUsers.get(i);
-			jsonObject.put("userName", user.getUserName());
-			jsonObject.put("login_ID", user.getLogin_ID());
-			jsonObject.put("email", user.getEmail());
-			jsonArray.add(jsonObject);
-		}
-		return jsonArray;
-	}
+        List<User> allUsers = new ArrayList<User>();
+        try {
+            log.info("开始链接：");
+            if (mks == null) {
+                mks = new MKSCommand();
+            }
+            allUsers = mks.getAllUsers(Arrays.asList("fullname", "name", "Email"));
+        } catch (APIException e) {
+            log.info("error: " + "查询所有用户错误！" + e.getMessage());
+            e.printStackTrace();
+        }
 
-	@RequestMapping(value = "/getAllUsersByProject", method = RequestMethod.POST)
-	public JSONArray getAllUsers(@RequestBody JSONObject jsonData) {
-		getToken(jsonData.getString("Access_Token"));
-		String project = jsonData.getString("project");
-		String type = jsonData.getString("type");// 根据类型判断获取的动态组，Component获取Review
-													// Committee Leader，其他获取
-		List<String> dynamicGroups = Arrays.asList(AnalysisXML.getTypeFilterGroup(type).split(","));
-		
+        // mks.close(host,port,loginName);
+
+        JSONArray jsonArray = new JSONArray();
+        for (int i = 0; i < allUsers.size(); i++) {
+            JSONObject jsonObject = new JSONObject();
+            User user = allUsers.get(i);
+            jsonObject.put("userName", user.getUserName());
+            jsonObject.put("login_ID", user.getLogin_ID());
+            jsonObject.put("email", user.getEmail());
+            jsonArray.add(jsonObject);
+        }
+        return jsonArray;
+    }
+
+    @RequestMapping(value = "/getAllUsersByProject", method = RequestMethod.POST)
+    public JSONArray getAllUsers(@RequestBody JSONObject jsonData) {
+        getToken(jsonData.getString("Access_Token"));
+        String project = jsonData.getString("project");
+        String type = jsonData.getString("type");// 根据类型判断获取的动态组，Component获取Review
+        // Committee Leader，其他获取
+        List<String> dynamicGroups = Arrays.asList(AnalysisXML.getTypeFilterGroup(type).split(","));
+
 //		if ("Component Requirement Specification Document".equals(type)) {// Component到In
 //			dynamicGroups.add("Project Team");
 ////			dynamicGroups.add("Review Committee Leader DG");
@@ -95,151 +97,147 @@ public class AlmController {
 //			dynamicGroups.add("Project Manager DG");
 //		}
 
-		log.info("project-----" + project);
-		// MKSCommand mks = new MKSCommand();
-		// mks.initMksCommand("192.168.120.128", 7001, "admin", "admin");
-		if (mks == null) {
-			mks = new MKSCommand();
-		}
-		List<User> allUsers = new ArrayList<User>();
-		try {
-			allUsers = mks.getProjectDynaUsers(project, dynamicGroups);
-		} catch (APIException e) {
-			log.error("error: " + "查询所有用户错误！" + e.getMessage());
-			e.printStackTrace();
-		}
+        log.info("project-----" + project);
+        // MKSCommand mks = new MKSCommand();
+        // mks.initMksCommand("192.168.120.128", 7001, "admin", "admin");
+        if (mks == null) {
+            mks = new MKSCommand();
+        }
+        List<User> allUsers = new ArrayList<User>();
+        try {
+            allUsers = mks.getProjectDynaUsers(project, dynamicGroups);
+        } catch (APIException e) {
+            log.error("error: " + "查询所有用户错误！" + e.getMessage());
+            e.printStackTrace();
+        }
 
-		JSONArray jsonArray = new JSONArray();
-		for (int i = 0; i < allUsers.size(); i++) {
-			JSONObject jsonObject = new JSONObject();
-			User user = allUsers.get(i);
-			jsonObject.put("userName", user.getUserName());
-			jsonObject.put("login_ID", user.getLogin_ID());
-			jsonObject.put("email", user.getEmail());
-			jsonArray.add(jsonObject);
-		}
-		return jsonArray;
-	}
+        JSONArray jsonArray = new JSONArray();
+        for (int i = 0; i < allUsers.size(); i++) {
+            JSONObject jsonObject = new JSONObject();
+            User user = allUsers.get(i);
+            jsonObject.put("userName", user.getUserName());
+            jsonObject.put("login_ID", user.getLogin_ID());
+            jsonObject.put("email", user.getEmail());
+            jsonArray.add(jsonObject);
+        }
+        return jsonArray;
+    }
 
-	/**
-	 * @Description
-	 * @Author liuxiaoguang
-	 * @Date 2020/7/17 14:53
-	 * @Param []
-	 * @Return com.alibaba.fastjson.JSONObject
-	 * @Exception 获取ALM中Project列表
-	 */
-	@RequestMapping(value = "/getAllProjects", method = RequestMethod.POST)
-	public JSONArray getAllProject(@RequestBody JSONObject jsonData) {
-		getToken(jsonData.getString("Access_Token"));
-		log.info("-------------查询所用项目-------------");
-		if (mks == null) {
-			mks = new MKSCommand();
-		}
-		List<Project> allUsers = new ArrayList<Project>();
-		try {
-			// mks.initMksCommand(host, port, loginName, passWord);
-			// mks.initMksCommand("192.168.120.128", 7001, "admin", "admin");
-			allUsers = mks.getAllprojects(Arrays.asList("backingIssueID", "name"));
-		} catch (APIException e) {
-			log.error("error: " + "查询所有project错误！" + e.getMessage());
-			e.printStackTrace();
-		}
+    /**
+     * @Description
+     * @Author liuxiaoguang
+     * @Date 2020/7/17 14:53
+     * @Param []
+     * @Return com.alibaba.fastjson.JSONObject
+     * @Exception 获取ALM中Project列表
+     */
+    @RequestMapping(value = "/getAllProjects", method = RequestMethod.POST)
+    public JSONArray getAllProject(@RequestBody JSONObject jsonData) {
+        getToken(jsonData.getString("Access_Token"));
+        log.info("-------------查询所用项目-------------");
+        if (mks == null) {
+            mks = new MKSCommand();
+        }
+        List<Project> allUsers = new ArrayList<Project>();
+        try {
+            // mks.initMksCommand(host, port, loginName, passWord);
+            // mks.initMksCommand("192.168.120.128", 7001, "admin", "admin");
+            allUsers = mks.getAllprojects(Arrays.asList("backingIssueID", "name"));
+        } catch (APIException e) {
+            log.error("error: " + "查询所有project错误！" + e.getMessage());
+            e.printStackTrace();
+        }
 
-		// mks.close(host,port,loginName);
+        // mks.close(host,port,loginName);
 
-		JSONArray jsonArray = new JSONArray();
-		for (int i = 0; i < allUsers.size(); i++) {
-			JSONObject jsonObject = new JSONObject();
-			Project project = allUsers.get(i);
-			jsonObject.put("project", project.getProject());
-			jsonObject.put("PID", project.getPID());
-			jsonArray.add(jsonObject);
-		}
-		return jsonArray;
-	}
+        JSONArray jsonArray = new JSONArray();
+        for (int i = 0; i < allUsers.size(); i++) {
+            JSONObject jsonObject = new JSONObject();
+            Project project = allUsers.get(i);
+            jsonObject.put("project", project.getProject());
+            jsonObject.put("PID", project.getPID());
+            jsonArray.add(jsonObject);
+        }
+        return jsonArray;
+    }
 
-	/**
-	 * @Description
-	 * @Author liuxiaoguang
-	 * @Date 2020/7/22 10:02
-	 * @Param [jsonData]
-	 * @Return com.alibaba.fastjson.JSONObject
-	 * @Exception 创建 修改 删除 移动文档条目
-	 */
-	@RequestMapping(value = "/releaseData", method = RequestMethod.POST)
-	public JSONObject createDocument(@RequestBody JSONObject jsonData) {
-		getToken(jsonData.getString("Access_Token"));
-		
-		// docID = ""; //文档id
-		if (mks == null) {
-			mks = new MKSCommand();
-		}
-		String docId = "";
+    /**
+     * @Description
+     * @Author liuxiaoguang
+     * @Date 2020/7/22 10:02
+     * @Param [jsonData]
+     * @Return com.alibaba.fastjson.JSONObject
+     * @Exception 创建 修改 删除 移动文档条目
+     */
+    @RequestMapping(value = "/releaseData", method = RequestMethod.POST)
+    public JSONObject createDocument(@RequestBody JSONObject jsonData) {
+        getToken(jsonData.getString("Access_Token"));
+
+        // docID = ""; //文档id
+        if (mks == null) {
+            mks = new MKSCommand();
+        }
 //		log.info(jsonData);
-		String docUUID = jsonData.getString(Constants.DOC_UUID);
-		String end = jsonData.get("end").toString();// 结尾标记，标识本次文档数据传输完毕
+        String docUUID = jsonData.getString(Constants.DOC_UUID);
+        String end = jsonData.get("end").toString();// 结尾标记，标识本次文档数据传输完毕
+        // 移动到父id下面的位置 参数 first last before:name after:name
+        // String insertLocation = jsonData.get("insertLocation"));
+        if ("true".equals(end)) { // 所有参数存入缓存
+            log.info("-------------数据下发 缓存完毕 UUID " + docUUID + "-------------");
+            MapCache.cacheVal(docUUID, jsonData);
+            List<JSONObject> listData = MapCache.getList(docUUID);// 保存排序后条目数据
+            MapCache.clearCache(docUUID);
 
-		// 移动到父id下面的位置 参数 first last before:name after:name
-		// String insertLocation = jsonData.get("insertLocation"));
-		if ("true".equals(end)) { // 所有参数存入缓存
-			log.info("-------------数据下发 缓存完毕 UUID " + docUUID + "-------------");
-			MapCache.cacheVal(docUUID, jsonData);
-			List<JSONObject> listData = MapCache.getList(docUUID);// 保存排序后条目数据
-			MapCache.clearCache(docUUID);
-			try {
-				IntegrityCallable call = new IntegrityCallable(listData);
-				FutureTask<String> futureTask = new FutureTask<>(call);
-				new Thread(futureTask).start();
-				docId = futureTask.get();
-			} catch (InterruptedException e) {
-				log.error("多线程错误：" + e.getMessage());
-				e.printStackTrace();
-				throw new MsgArgumentException("210",e.getMessage());
-			} catch(MsgArgumentException e){
-				log.error("错误：" + e.getMessage());
-				throw e;
-			} catch (ExecutionException e) {
-				log.error("多线程执行错误：" + e.getMessage());
-				e.printStackTrace();
-				throw new MsgArgumentException("210","多线程执行错误" + e.getMessage());
-			}
-		} else {
-			log.info("-------------数据下发 缓存中 UUID " +docUUID+ "-------------");
-			MapCache.cacheVal(docUUID, jsonData);
-//			log.info(ResultJson("data", ""));
-			return ResultJson("data", "");
-		}
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("data", ResultJson("DOC_ID", docId));
-		return jsonObject;
-	}
+            String info = util.checkData(listData);
+            log.warn("校验数据：" + info);
+            if (!"success".equals(info)) {
+                return ResultJson("data", info);
+            }
+            try {
+                new Thread() {
+                    @SneakyThrows
+                    @Override
+                    public void run() {
+                        util.dealData(listData);
+                    }
+                }.start();
+            } catch (Exception e) {
+                log.error("多线程错误：" + e.getMessage());
+                throw new MsgArgumentException("210", e.getMessage());
+            }
+        } else {
+            log.info("-------------数据下发 缓存中 UUID " + docUUID + "-------------");
+            MapCache.cacheVal(docUUID, jsonData);
+            return ResultJson("data", "");
+        }
+        return ResultJson("data", "");
+    }
 
-	// 变更反馈增删改条目
-	@RequestMapping(value = "/changeAction", method = RequestMethod.POST)
-	public JSONObject changeAction1(@RequestBody JSONObject jsonData) {
-		getToken(jsonData.getString("Access_Token"));
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("data", ResultJson("DOC_ID", util.changeExecution(jsonData)));
-		log.info("变更完成：");
-		return jsonObject;
-	}
+    // 变更反馈增删改条目
+    @RequestMapping(value = "/changeAction", method = RequestMethod.POST)
+    public JSONObject changeAction1(@RequestBody JSONObject jsonData) {
+        getToken(jsonData.getString("Access_Token"));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("data", ResultJson("DOC_ID", util.changeExecution(jsonData)));
+        log.info("变更完成：");
+        return jsonObject;
+    }
 
 
-	/**
-	 * token验证
-	 */
-	public void getToken(String str) {
-		if (token.equals(str)) {
-			return;
-		} else {
-			log.error("token验证失败!");
-			throw new MsgArgumentException("201", "token Validation failed!");
-		}
-	}
-	
+    /**
+     * token验证
+     */
+    public void getToken(String str) {
+        if (token.equals(str)) {
+            return;
+        } else {
+            log.error("token验证失败!");
+            throw new MsgArgumentException("201", "token Validation failed!");
+        }
+    }
 
-	public static void main(String[] str) {
-		
-	}
+
+    public static void main(String[] str) {
+
+    }
 }
